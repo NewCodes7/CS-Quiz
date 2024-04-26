80f83d3c4101babdd2857746ce611631405da32d
H-server <hani103201@naver.com>
2024-04-26 10:15:43 +0900

    프로젝트 세팅

---
+++ src/main/java/newcodes/CSQuiz/CsQuizApplication.java
@@ -0,0 +1,13 @@
+package newcodes.CSQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsQuizApplication.class, args);
+	}
+
+	}
