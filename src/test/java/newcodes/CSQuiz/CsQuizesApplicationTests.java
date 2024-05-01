bc08602fb470c2ed358fe4c5646e6c9f1c4665ad
H-server <hani103201@naver.com>
2024-05-01 11:25:46 +0900

    feat: JWT 토큰 발급 기능 구현

--- src/test/java/newcodes/CSQuiz/CsQuizesApplicationTests.java
+++ src/test/java/newcodes/CSQuiz/CsQuizesApplicationTests.java
@@ -1,10 +1,10 @@
-package newcodes.CSQuizes;
+package newcodes.CSQuiz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CsQuizesApplicationTests {
+class CsQuizApplicationTests {
 
 	@Test
 	void contextLoads() {
