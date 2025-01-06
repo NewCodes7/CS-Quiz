11a823cfb7075064da56ea0f0d4cf03bb89acddc
NewCodes7 <newcodes7@gmail.com>
2025-01-06 16:01:22 +0900

    test: 퀴즈 단일 조회 서비스 단위 테스트 코드 작성

---
+++ src/test/java/newcodes/CSQuiz/quiz/service/QuizServiceTest.java
@@ -0,0 +1,49 @@
+package newcodes.CSQuiz.quiz.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import newcodes.CSQuiz.exception.QuizNotFoundException;
+import newcodes.CSQuiz.quiz.domain.Quiz;
+import newcodes.CSQuiz.quiz.repository.QuizRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class QuizServiceTest {
+
+    @Mock
+    private QuizRepository quizRepository;
+
+    @InjectMocks
+    private QuizService quizService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("퀴즈 ID를 통해 단일 퀴즈를 조회할 수 있다")
+    void findQuizByIdReturnsQuizWhenQuizIsFound() {
+        Quiz quiz = Quiz.builder().quizId(1).questionText("question").build();
+        when(quizRepository.findQuizById(1)).thenReturn(Optional.of(quiz));
+
+        Quiz result = quizService.findQuizById(1);
+
+        assertEquals(quiz, result);
+    }
+
+    @Test
+    @DisplayName("퀴즈 ID로 조회시 퀴즈가 존재하지 않으면 QuizNotFoundException이 발생한다")
+    void findQuizByIdThrowsQuizNotFoundExceptionWhenQuizIsNotFound() {
+        when(quizRepository.findQuizById(1)).thenReturn(Optional.empty());
+
+        assertThrows(QuizNotFoundException.class, () -> quizService.findQuizById(1));
+    }
+}
